bd857e04b8ad4aea0aa85c40e7d2176f7a804a2b
Jorge Bucaran <jbucaran@me.com>
2015-06-13 08:14:17 +0900

    use default wa-git funcs

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,15 @@
-function fst; set_color -o 90f; end
-function snd; set_color -o cF3; end
-function trd; set_color -o fff; end
-function dim; set_color -o 555; end
-function err; set_color -o f30; end
+function hulk::fst; set_color -o 90f; end
+function hulk::snd; set_color -o cF3; end
+function hulk::trd; set_color -o fff; end
+function hulk::dim; set_color -o 555; end
+function hulk::err; set_color -o f30; end
 function off; set_color normal; end
 
 function fish_prompt
   set -l code $status
-  function status::color -S
-    test $code -ne 0; and err; or snd
+  function hulk::status::color -S
+    test $code -ne 0; and hulk::err; or hulk::snd
   end
-  printf (dim)(date +%H(status::color):(dim)%M)(off)
-  printf (status::color)" ≡ "(off)
+  printf (hulk::dim)(date +%H(hulk::status::color):(hulk::dim)%M)(off)
+  printf (hulk::status::color)" ≡ "(off)
 end
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,31 +1,23 @@
-function git::branch_name
-  command git symbolic-ref --short HEAD | tr '[:lower:]' '[:upper:]'
-end
-
-function git::is_touched
-  test -n (echo (command git status --porcelain))
-end
-
 function fish_right_prompt
   set -l code $status
 
-  function status::color -S
-    test $code -ne 0; and echo (err); or echo (fst)
+  function hulk::status::color -S
+    test $code -ne 0; and echo (hulk::err); or echo (hulk::fst)
   end
 
   set -l base (basename "$PWD")
 
   if test "$PWD" != "/"
-    prompt_pwd | sed "s|~|"(status::color)"ᴦ"(off)"|g" \
-    | sed "s|/|"(status::color)"/"(off)(dim)"|g" \
-    | sed "s|$base|"(trd)" $base"(off)"|g"
+    prompt_pwd | sed "s|~|"(hulk::status::color)"ᴦ"(off)"|g" \
+    | sed "s|/|"(hulk::status::color)"/"(off)(hulk::dim)"|g" \
+    | sed "s|$base|"(hulk::trd)" $base"(off)"|g"
   end
 
   if test -d .git
-    echo (status::color)" ≡ "(snd)(begin
-      git::is_touched; and git::branch_name; or echo (dim)(git::branch_name)
+    echo (hulk::status::color)" ≡ "(hulk::snd)(begin
+      git_is_touched; and git_branch_name; or echo (hulk::dim)(git_branch_name)
     end)(off)
   else
-    echo (status::color)" ≡"(off)
+    echo (hulk::status::color)" ≡"(off)
   end
 end
