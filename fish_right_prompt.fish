aa0fc4901eaa642e9b55004415191d90dc3fcc7d
Jorge Bucaran <j@bucaran.me>
2016-01-22 23:59:51 +0900

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,7 @@
+function git_is_repo -d "Check if directory is a repository"
+  test -d .git; or command git rev-parse --git-dir >/dev/null ^/dev/null
+end
+
 function git_branch_name -d "Get current branch name"
   git_is_repo; and begin
     command git symbolic-ref --short HEAD
